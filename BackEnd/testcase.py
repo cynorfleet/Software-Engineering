2c4705781595303fa32f229119852ca27294ebec
Christian Norfleet <cynorfleet@gmail.com>
2017-04-01 19:14:15 -0500

    added line number parsing
    also created blacklist to remove items like 'int main()'

--- BackEnd/testcase.py
+++ BackEnd/testcase.py
@@ -1,45 +1,59 @@
 #!/usr/bin/env python
-import testformatter, re
+import re
 
-''' GLOBALS:        these variables will be used when FINDING KEYWORD in user code
-    strprse =       finds alphanumeric keyword (this includes array indicies)
-    inputdic =      specfies which keyword to look for '''
-strprse  = '*(([a-zA-Z0-9 ])*(\[.*\]))*[ ;]'
-inputdic = {'cin':re.compile('cin[ > ]{}'.format(strprse)),
-            'cout':re.compile('cout[ < ]{}'.format(strprse))}
+#   GLOBALS:        these variables will be used when FINDING KEYWORD in user code
+#   strprse =       finds alphanumeric keyword (this includes array indicies)
+#   inputdic =      specfies which keyword to look for
+closure = '[ ,;]'
+strprse  = '([a-zA-Z0-9 ]*([\[a-zA-Z0-9\]]*))'
+inputtype  = {'int':re.compile(' *(int[ ]+{}){}'.format(strprse, closure))}
+inputdic = {'cin':re.compile(' *cin[ >]*{}'.format(strprse)),
+            'getline':re.compile(' *getline[ \(]*{}\)'.format(strprse))}
+blacklist = ['int main']
 
-''' GETFILE:        this function will GET ALL LINES in a FILE '''
+# GETFILE:        this function will GET ALL LINES in a FILE
 def getFile(file):
     with open (file, "r") as myfile:    # open the file temporarily
         linelist=myfile.readlines()     # store lines as an array
     return linelist                     #return the array
 
-''' PARSELINE:      this function will FIND USER VARIABLES in each LINE '''
-def parseLine(list, keyword):
-    for i in list:                              # for every line in the file
-        results = re.search(keyword, i)         # search for var and put in group 1
-        if results != None:                     # if a match was found
-            results = results.group(1)          # throw away everythong but group 1
-            results = results.replace(' ','')   # remove whitespaces
-            return results
 
-''' SHOWMATCHES:    this function will PRINT the MATCHES'''
-def showMatches(matchtype, match):
-    print("{type} Found: {match}\n".format(type=matchtype, match=match))
+# PARSELINE:      this function will FIND USER VARIABLES in each LINE
+def parseLines(lines, keyword, strip=None):
+    resultlist = []
+    for i, word in enumerate(lines):                            # for every line in the file
+        result = re.match(keyword, word)         # search for var and put in group 1
+        if result != None:                     # if a match was found
+            result = result.group(1)
+            if strip != None:                   # throw away everythong but group 1
+                results = result.replace(' ','')   # remove whitespaces
+            if result not in blacklist:
+                resultlist.append((i+1,result))         # add result to a list
+    return resultlist
+
+
+# SHOWMATCHES:    this function will PRINT the MATCHES
+def showMatches(matchtype, matches, log):
+    log.write("--------------------------------------------\n")
+    log.write("{type} Found:\n".format(type=matchtype))   # print header
+    for i, match in enumerate(matches):             # for each match in list
+        log.write("{match}\n".format(match=matches[i]))   # print the match
     return
 
-''' MAIN:           AND HERE... WE... GO'''
+
+# MAIN:           AND HERE... WE... GO
 def main():
-    filepath=raw_input("Enter File Path: ")
+    log = open('inputs.log', 'w')
+    filepath = raw_input("Enter File Path: ")
     lines = getFile(filepath)
 
-    inputs = parseLine(lines, inputdic['cin'])
-    outputs = parseLine(lines, inputdic['cout'])
+    inputs = parseLines(lines, inputdic['cin'])
+    datatype = parseLines(lines, inputtype['int'])
 
-    showMatches('Inputs', inputs)
-    showMatches('Outputs', outputs)
+    showMatches('Inputs', inputs, log)
+    showMatches('Type', datatype, log)
 
-    print("DONE")
+    print("\nDONE")
     return 0
 
 if __name__ == '__main__':
