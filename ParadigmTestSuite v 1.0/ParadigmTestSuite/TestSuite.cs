f5efa83bd7dbde6b40e8bfbefb4873ce7b13d87f
LegendaryZReborn <cbrolegendz@gmail.com>
2017-04-14 00:26:19 -0500

    Paradigm Test Suite version 1.2 created for further development

---
+++ ParadigmTestSuite v 1.0/ParadigmTestSuite/TestSuite.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web;
+using System.IO;
+using System.Collections;
+
+namespace ParadigmTestSuite
+{
+    public partial class TestSuite : Form
+    {
+        TestCase testGen = new TestCase();
+        TestDriver testDriver = new TestDriver();
+
+        public TestSuite()
+        {
+            InitializeComponent();
+            //Keep users from using functionality not yet accessable
+            generateDriverButton.Enabled = false;
+            generateTestButton.Enabled = false;
+            saveConfigButton.Enabled = false;
+            saveReportButton.Enabled = false;
+            generateDriverToolStripMenuItem.Enabled = false;
+            generateTestToolStripMenuItem.Enabled = false;
+            saveConfigToolStripMenuItem.Enabled = false;
+            saveReportToolStripMenuItem.Enabled = false;
+        }
+
+        private void languageBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //If the user selects option other than C++ change option back to C++
+            if (languageBox.SelectedIndex != 0)
+            {
+                languageBox.SelectedIndex = -1;
+            }
+            checkSelected();
+        }
+
+        private void testMethod_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            checkSelected();        
+        }
+
+        private void testLevel_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            checkSelected();
+        }
+
+        private void testMethod_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+
+            if(e.NewValue == CheckState.Checked)
+            {
+                for(int x = 0; x < testMethod.Items.Count; x++)
+                {
+                    if (e.Index != x) testMethod.SetItemChecked(x, false);
+                }
+            }
+        }
+
+        private void openFileButton_Click(object sender, EventArgs e)
+        {
+            String[] filePaths;
+            filePaths = openSource();
+            if (filePaths.Length != 0)
+            {
+                testGen.readFile(filePaths[0]);
+                testDriver.readFile(filePaths[0]);
+                fileNameL.Text = filePaths[0];
+            }
+        }
+
+        //Purpose: populates the variable box with the input variables
+        //Requires: nothing
+        //Returns: nothing
+        private void populateVariableBox()
+        {
+            List<Variable> testCases = testGen.SourceInputs;
+            string i = "";
+
+            variableBox.Items.Clear();
+            foreach (Variable v in testCases)
+            {
+                i += v.type + " " + v.identifier;
+                variableBox.Items.Add(i);
+                i = "";
+            }
+        }
+
+        private void saveReportButton_Click(object sender, EventArgs e)
+        {
+            saveFile();
+        }
+
+        private void saveReportToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            saveFile();
+        }
+
+        private void saveFile()
+        {
+            Stream myStream;
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = saveFileDialog1.OpenFile()) != null)
+                {
+                    // Code to write the stream goes here.
+                    myStream.Close();
+                }
+            }
+        }
+
+        private void checkSelected()
+        {
+            if (languageBox.SelectedIndex != -1 && testMethod.SelectedIndex != -1 && testLevel.SelectedIndex != -1)
+            {
+                generateDriverButton.Enabled = true;
+                generateTestButton.Enabled = true;
+                saveConfigButton.Enabled = true;
+                saveReportButton.Enabled = true;
+                generateDriverToolStripMenuItem.Enabled = true;
+                generateTestToolStripMenuItem.Enabled = true;
+                saveConfigToolStripMenuItem.Enabled = true;
+                saveReportToolStripMenuItem.Enabled = true;
+            }
+            else
+            {
+                generateDriverButton.Enabled = false;
+                generateTestButton.Enabled = false;
+                saveConfigButton.Enabled = false;
+                saveReportButton.Enabled = false;
+                generateDriverToolStripMenuItem.Enabled = false;
+                generateTestToolStripMenuItem.Enabled = false;
+                saveConfigToolStripMenuItem.Enabled = false;
+                saveReportToolStripMenuItem.Enabled = false;
+            }
+        }
+
+        private string[] openSource()
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+           // openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "C++ files (*.cpp)|*.cpp|header files (*.h)|*.h|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 3;
+            openFileDialog1.RestoreDirectory = true;
+            openFileDialog1.ShowDialog();
+
+            return openFileDialog1.FileNames;
+        }
+
+        private void openConfig()
+        {
+            languageBox.SelectedIndex = Properties.Settings.Default.TestLang;
+            testMethod.SetItemCheckState(Properties.Settings.Default.TestMeth, CheckState.Checked);
+            testLevel.SelectedIndex = Properties.Settings.Default.TestInt;
+        } 
+
+        private void saveConfig()
+        {
+            Properties.Settings.Default.TestLang = languageBox.SelectedIndex;
+            Properties.Settings.Default.TestMeth = testMethod.SelectedIndex;
+            Properties.Settings.Default.TestInt = testLevel.SelectedIndex;
+            Properties.Settings.Default.Save();
+        }
+        
+        private void loadConfigButton_Click(object sender, EventArgs e)
+        {
+            openConfig();
+        }
+
+        private void saveConfigButton_Click(object sender, EventArgs e)
+        {
+            saveConfig();
+        }
+
+        private void openConfigToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            openConfig();
+        }
+
+        private void generateTest()
+        {
+            //Load the configuration into a configuration class
+            Configuration testConfig = new Configuration();
+            testConfig.TestType = testMethod.SelectedIndex;
+            testConfig.TestIntensity = testLevel.SelectedIndex;
+
+            ArrayList testCases;
+            //Make the test variables
+            testCases = testGen.generateTest(testConfig);
+            //Populate the variable list
+            populateVariableBox();
+            //Populate the input box with the generated test cases
+            populateInputBox(testCases);
+        }
+
+        private void generateTestButton_Click(object sender, EventArgs e)
+        {
+            generateTest();
+        }
+
+        private void generateTestToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            generateTest();
+        }
+
+        private void populateInputBox(ArrayList testCases)
+        {
+           
+            string s = "";
+
+            inputBox.Items.Clear();
+            foreach (ArrayList l in testCases)
+            {
+                foreach(var i in  l)
+                {
+                    s += (i.ToString() + " ");
+                }
+                inputBox.Items.Add(s);
+                s = "";
+            }
+        }
+
+        private void generateDriverButton_Click(object sender, EventArgs e)
+        {
+            string driver;
+            driver = testDriver.generateDriver();
+            driverBox.Text = driver;
+        }
+
+        private void generateDriverToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aboutToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Paradigm Test Suite" + "\n" + "Version 1" + "\n" + "Test case generator", "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void userManualToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void closeToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void HandEnter_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Once the user has entered something and hit enter
+            if (e.KeyCode == Keys.Enter && !String.IsNullOrWhiteSpace(HandEnter.Text))
+            {
+                //Add the item to the end of the list
+                HandResults.Items.Add(HandEnter.Text);
+                HandEnter.Clear();
+                int thisIndex = HandResults.Items.Count - 1;
+                //And compare it to the item in the other list if it has been entered.
+                if(ProgramResults.Items.Count >= HandResults.Items.Count)
+                {
+                    if (HandResults.Items[thisIndex].ToString() == ProgramResults.Items[thisIndex].ToString())
+                    {
+                        PassFail.Items.Add('√');
+                    }
+                    else
+                    {
+                        PassFail.Items.Add('X');
+                    }
+                }
+            }
+        }
+
+        private void ProgramEnter_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Once the user has entered something and hit enter
+            if (e.KeyCode == Keys.Enter && !String.IsNullOrWhiteSpace(ProgramEnter.Text))
+            {
+                //Add the item to the end of the list
+                ProgramResults.Items.Add(ProgramEnter.Text);
+                ProgramEnter.Clear();
+                int thisIndex = ProgramResults.Items.Count - 1;
+                //And compare it to the item in the other list if it has been entered.
+                if (HandResults.Items.Count >= ProgramResults.Items.Count)
+                {
+                    if (HandResults.Items[thisIndex].ToString() == ProgramResults.Items[thisIndex].ToString())
+                    {
+                        PassFail.Items.Add('√');
+                    }
+                    else
+                    {
+                        PassFail.Items.Add('X');
+                    }
+                }
+            }
+        }
+
+       
+    }
+}
---
+++ ParadigmTestSuite v 1.2/ParadigmTestSuite/TestDriver.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+
+namespace ParadigmTestSuite
+{
+    public struct Function
+    {
+        
+        public Function(string type, string fName)
+        {
+            parameterList = new List<string>();
+            returnType = type;
+            name = fName;
+        }
+
+        public string returnType;
+        public string name;
+        public List<string> parameterList;
+    }
+
+
+    public class TestDriver
+    {
+        private string className = "Point";
+        private List<Function> functions;
+        private string pythonOutfile;
+        Random rand;
+
+        public TestDriver()
+        {
+            functions = new List<Function>();
+            pythonOutfile = "data";
+            rand = new Random();
+        }
+
+        public void readFile(string sourceFileName)
+        {
+            List<String> usr_functs = new List<String>();
+            List<String> usr_declarations = new List<String>();
+            List<String> usr_inputs = new List<String>();
+
+            runPythonScript(sourceFileName, usr_functs, usr_declarations, usr_inputs);
+            
+            //parse function data
+            parseMethods(usr_functs);
+        }
+
+      
+
+        //Purpose:Runs a python script that will parse the source file and write data about inputs,
+        //and functions to an output file called data.dat.
+        //Requires: The source file name 
+        //Returns: nothing
+        private void runPythonScript(string sourceFileName, List<String> usr_functs,
+        List<String> usr_declarations, List<String> usr_inputs)
+        {
+            List<string> argv = new List<string>();
+            IronPython.Runtime.List functions = new IronPython.Runtime.List();
+            IronPython.Runtime.List declarations = new IronPython.Runtime.List();
+            IronPython.Runtime.List userinputs = new IronPython.Runtime.List();
+
+            argv.Add(sourceFileName);
+            argv.Add(pythonOutfile);
+
+            var engine = Python.CreateEngine(); // Extract Python language engine from their grasp
+            var scope = engine.GetSysModule();
+            scope.SetVariable("argv", argv);
+            ScriptSource source = engine.CreateScriptSourceFromFile(@"testcase.py"); // Load the script
+            object result = source.Execute(scope);
+
+            //gets specific variables from the python script
+            functions.extend(scope.GetVariable("functions"));
+            declarations.extend(scope.GetVariable("datatype"));
+            userinputs.extend(scope.GetVariable("userinputs"));
+
+            snatchData(functions, usr_functs);
+            snatchData(declarations, usr_declarations);
+            snatchData(userinputs, usr_inputs);
+
+
+        }
+
+        //Purpose:Runs a python script that will parse the source file and write data about inputs,
+        //and functions to an output file called data.dat.
+        //Requires: The source file name, List<String> usr_functs,
+        //  List<String> usr_declarations, List<String> usr_inputs
+        //Returns: nothing
+        private void snatchData(IronPython.Runtime.List raw_data, List<String> new_data)
+        {
+            foreach (var i in raw_data)
+                new_data.Add(i.ToString());
+        }
+
+
+        private void parseMethods(List<String> usr_functs)
+        {
+            Regex reg = new Regex(@"\w+");
+            Function f = new Function("", "");
+            int start;
+            //list for storing regex matchesof words in usr_functs elements
+            List<string> l = new List<string>();
+
+
+            functions.Clear(); // clears list 
+
+            f.name = "Point";
+            functions.Add(f);
+
+            foreach(string s in usr_functs)
+            {
+                foreach (Match m in reg.Matches(s))
+                {
+                    l.Add(m.Value);
+                    
+                }
+
+                if (l[0] == "void" || l[0] == "char" || l[0] == "int" ||
+                    l[0] == "string" || l[0] == "double" || l[0] == "float")
+                {
+                    f = new Function(l[0], l[1]);
+                    start = 2;
+                }
+                else
+                {
+                    f = new Function("", l[0]);
+                    start = 1;
+                }
+                
+                for(int i = start; i < l.Count; i++)
+                {
+                    if (l[i] == "char" || l[i] == "int" ||
+                        l[i] == "string" || l[i] == "double" || l[i] == "float")
+                    {
+                        f.parameterList.Add(l[i]);
+                    }
+                }
+
+                functions.Add(f);
+                l.Clear();
+
+            } // end foreach
+
+        }
+
+        //Purpose: Generates test driver to be suggested to the user
+        //Requires:
+        //Returns: a string - a test driver script
+        public string generateDriver()
+        {
+            string driver = "#include <iostream>\n";
+            driver += "#include <string>\n";
+            driver += String.Format("#include\"{0}\" \n", className);
+
+            driver +=
+            @"using namepace std;
+            
+int main()
+{
+";
+            string vars;
+            // instantiate an appropriate object
+            vars = className + " classObject;\n";
+            driver += vars;
+
+            bool first;
+            int varCount = 0;
+            //create a call for each class method
+            foreach (Function f in functions)
+            {
+                //create parameterized constructor object
+                if (f.name == className && f.parameterList != null && f.parameterList.Count != 0)
+                {                   
+                    vars = className + " classObject(";
+
+                    first = true;
+                    //generate and concatenate a value for each parameter 
+                    foreach (string s in f.parameterList)
+                    {
+                        if (!first)
+                            vars += ", ";
+
+                        vars += randomDatGen(s);
+                        first = false;
+
+                    }
+                    vars += ");\n";
+                    driver += vars;
+                  
+                }//end if
+
+                if(f.name != className)
+                {
+                    if (f.returnType == "int")
+                    {
+                        vars = "int var" + varCount + ";\n";
+                        driver += vars;
+                        varCount++;
+                    }
+                    else if (f.returnType == "char")
+                    {
+                        vars = "char var" + varCount + ";\n";
+                        driver += vars;
+                        varCount++;
+                    }
+                    else if (f.returnType == "double" || f.returnType == "float")
+                    {
+                        vars = "double var" + varCount + ";\n";
+                        driver += vars;
+                        varCount++;
+                    }
+
+                    else if (f.returnType == "string")
+                    {
+                        vars = "string var" + varCount + ";\n";
+                        driver += vars;
+                        varCount++;
+                    }
+
+
+                    vars = "";
+                    //if the return type isnt void, make the function call
+                    //and have it return to the right variable
+                    if (f.returnType != "void")
+                    {
+                        vars = "var" + Convert.ToString(varCount - 1) + " = ";
+                                     
+                    }
+
+                    vars += ("classObject." + f.name + "(");
+
+                    first = true;
+                    foreach (string m in f.parameterList)
+                    {
+                        if (!first)
+                            vars += ", ";
+                      
+                        //quotes go around strings and chars
+                        if (m == "string")
+                            vars += "\"" + randomDatGen(m).ToString() + "\"";
+                        else if(m == "char")
+                            vars += "\'" + randomDatGen(m).ToString() + "\'";
+                        else
+                            vars += randomDatGen(m).ToString();
+
+                        first = false;
+                    }
+
+                    vars += ");\n";
+                    driver += vars;
+                }//endif
+
+            }// end foreach
+
+            vars = @"return 0;
+}";
+            driver += vars;
+
+            return driver;
+        }
+
+        //Purpose: Generates random data
+        //Requires: string type
+        //Returns: string 
+        private string randomDatGen(string type)
+        {
+            string randomDat = "";
+         
+            if (type == "int")
+            {
+                int randomInt;
+                int one;
+               
+                //generate 1 or -1 randomly
+                one = rand.Next(-1, 2);
+                while (one == 0)
+                    one = rand.Next(-1, 1);
+
+                //gets a random integer (can be negative)
+                randomInt = rand.Next(0, 11) * one;
+                randomDat = randomInt.ToString();
+                                  
+            }
+            else if (type == "char")
+            {
+                char randomChar;
+
+                //generate random char of ascii values between 32 and 127
+                randomChar = Convert.ToChar(rand.Next(32, 128));
+                randomDat = randomChar.ToString();
+            }
+            else if (type == "double" || type == "float")
+            {
+                double randomFloat;
+
+                //generate a number of random floating point numbers                   
+                randomFloat = rand.NextDouble() * (10 - (-10)) + (-10);
+                randomDat = randomFloat.ToString();                               
+            }
+
+            else if (type == "string")
+            {
+                string randomString = "";
+                char randomChar;
+                int sLength;
+
+                  
+                //get a random string length
+                sLength = rand.Next(0, 10);
+
+                //generate a random string by generating sLength random chars
+                for (int l = 0; l < sLength; l++)
+                {
+                    //generate random char of ascii values between 32 and 127
+                    randomChar = Convert.ToChar(rand.Next(65, 127));
+                    randomString += randomChar; ;
+                }
+                 randomDat = randomString;
+
+            }
+            return randomDat;
+        }
+
+        //Property that returns the list of source code functions
+        public List<Function> SourceFunctions
+        {
+            get { return functions; }
+        }
+
+    }
+}
