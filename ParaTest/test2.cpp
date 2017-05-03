f69a3cd63e5a221129dc85051b768c1c99b0e930
Cavaughn Browne <cbrolegendz@gmail.com>
2017-05-03 17:16:40 -0500

    Add files via upload

---
+++ ParaTest/test2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	int testInt;
+	char testChar;
+	string testString;
+
+	cin >> testInt;
+	cin >> testChar >> testString;
+	//cin >> testInt;
+}
